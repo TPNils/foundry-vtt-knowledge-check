44196d387eddaf21fc0f4239db3f7ba075120d5f
Nils <nils.yseboodt@gmail.com>
2021-05-23 07:17:47 +0200

    implement identifying abilities

---
+++ src/scripts/hook-manager.ts
@@ -0,0 +1,108 @@
+import { Identifiable } from "./identifiable.js";
+import { staticValues } from "./static-values.js";
+
+interface ItemSheetHeaderButton {
+  class: string;
+  icon: string;
+  label: string;
+  onclick: (event: MouseEvent) => void
+}
+
+export class HookManager {
+  public static registerAll(): void {
+    Hooks.on('getItemSheetHeaderButtons', (...args: any) => HookManager.getItemSheetHeaderButtons(args[0], args[1]));
+    Hooks.on('init', () => HookManager.init());
+    Hooks.on('ready', () => HookManager.ready());
+  }
+
+  private static init(): void {
+    game[staticValues.moduleName] = {
+      printAbilities: Identifiable.printAbilities
+    };
+  }
+
+  private static ready(): void {
+    // Add a listener to detect clicking on the checkbox input
+    document.querySelector('#chat').addEventListener('change', event => {
+      if (event.target instanceof HTMLInputElement) {
+        if (event.target.classList.contains('knowledge-check') && event.target.classList.contains('identifiable')) {
+          let actorId: string;
+          let ownedItemId: string;
+          let messageId: string;
+  
+          let messageRoot: HTMLElement;
+          let element: HTMLElement = event.target;
+          while (element.parentElement && (!actorId || !ownedItemId || !messageRoot)) {
+            if (!actorId && element.dataset.actorId) {
+              actorId = element.dataset.actorId;
+            }
+            if (!ownedItemId && element.dataset.ownedItemId) {
+              ownedItemId = element.dataset.ownedItemId;
+            }
+            if (!messageId && element.dataset.messageId) {
+              messageId = element.dataset.messageId;
+              messageRoot = element;
+            }
+            element = element.parentElement;
+          }
+  
+          if (!actorId || !ownedItemId || !messageId) {
+            return;
+          }
+  
+          event.target.setAttribute('checked', "");
+          messageRoot.querySelectorAll('input').forEach(element => {
+            element.disabled = true;
+          });
+  
+          Identifiable.setRevealed(game.actors.get(actorId).items.get(ownedItemId), true);
+          ChatMessage.update({
+            _id: messageId,
+            content: messageRoot.querySelector('.message-content').innerHTML
+          });
+        }
+      }
+  
+    })
+  }
+
+  private static getItemSheetHeaderButtons(app: any, buttons: ItemSheetHeaderButton[]): void {
+    if (!game.user.hasRole(CONST.USER_ROLES.ASSISTANT)) {
+      return;
+    }
+    const item: Item<any> = app.item;
+    buttons.unshift({
+      class: `${staticValues.moduleName}-toggle-identifiable`,
+      icon: HookManager.getItemSheetHeaderButtonIcon(item),
+      label: 'Identifiable',
+      onclick: async () => {
+        await Identifiable.setIdentifiable(item, !Identifiable.isIdentifiable(item));
+        document.querySelectorAll(`.${staticValues.moduleName}-toggle-identifiable i`).forEach(icon => {
+          icon.className = HookManager.getItemSheetHeaderButtonIcon(item);
+        });
+      }
+    })
+
+    // reset with right click
+    setTimeout(() => {
+      document.querySelectorAll(`.${staticValues.moduleName}-toggle-identifiable:not(.${staticValues.moduleName}-has-context-listener)`).forEach(element => {
+        element.classList.add(`${staticValues.moduleName}-has-context-listener`);
+        element.addEventListener('contextmenu', async () => {
+          await Identifiable.setIdentifiable(item, null);
+          element.querySelectorAll(`:scope i`).forEach(icon => {
+            icon.className = HookManager.getItemSheetHeaderButtonIcon(item);
+          });
+        })
+      });
+    }, 500);
+  }
+
+  private static getItemSheetHeaderButtonIcon(item: Item<any>): string {
+    if (Identifiable.isManuallySet(item)) {
+      return Identifiable.isIdentifiable(item) ? 'fas fa-check' : 'fas fa-times';
+    } else {
+      return Identifiable.isIdentifiable(item) ? 'fas fa-check-circle' : 'fas fa-times-circle';
+    }
+  }
+
+}
---
+++ src/scripts/identifiable.ts
@@ -0,0 +1,135 @@
+import { staticValues } from "./static-values.js";
+
+export interface IdentifiableAbility {
+  actorId: string;
+  ownedItemId: string;
+  name: string;
+  img: string;
+  revealed: boolean;
+}
+
+export class Identifiable {
+
+  public static isIdentifiable(item: Item<any>): boolean {
+    const isIdentifiable = item.getFlag(staticValues.moduleName, 'identifiable');
+    if (isIdentifiable == null) {
+      // fallback for dnd5e
+      if (item.type === 'spell') {
+        // what spells the character knows is still a secret and are usualy vanilla dnd spells
+        return false;
+      }
+      if (item.type === 'weapon' || item.type === 'equipment' || item.type === 'feat') {
+        if (item.data.data?.activation?.type != null && item.data.data?.activation?.type !== '') {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      
+      // (as fallback) Don't show the following
+      // consumable, tool, loot, class, backpack
+      return false;
+    }
+    
+    switch (typeof isIdentifiable) {
+      case 'boolean':
+        return isIdentifiable;
+      case 'bigint':
+      case 'number':
+        return isIdentifiable > 0;
+      case 'string':
+        if ('true' === isIdentifiable.toLowerCase()) {
+          return true;
+        }
+        if ('false' === isIdentifiable.toLowerCase()) {
+          return false;
+        }
+      default: 
+        return !!isIdentifiable;
+    }
+  }
+
+  public static isManuallySet(item: Item<any>): boolean {
+    return item.getFlag(staticValues.moduleName, 'identifiable') == null;
+  }
+
+  public static async setIdentifiable(item: Item<any>, value: boolean | null): Promise<void> {
+    if (value == null) {
+      await item.unsetFlag(staticValues.moduleName, 'identifiable');
+    } else {
+      await item.setFlag(staticValues.moduleName, 'identifiable', value);
+    }
+
+    // Reset reveal, a 'it works well enough' solution to allow to reset something that was revealed
+    if (!value) {
+      const isRevealed = item.getFlag(staticValues.moduleName, 'revealed');
+      console.log(isRevealed);
+      if (isRevealed != null) {
+        await Identifiable.setRevealed(item, null);
+      }
+    }
+  }
+
+  public static isRevealed(item: Item<any>): boolean {
+    const isRevealed = item.getFlag(staticValues.moduleName, 'revealed');
+    
+    switch (typeof isRevealed) {
+      case 'boolean':
+        return isRevealed;
+      case 'bigint':
+      case 'number':
+        return isRevealed > 0;
+      case 'string':
+        if ('true' === isRevealed.toLowerCase()) {
+          return true;
+        }
+        if ('false' === isRevealed.toLowerCase()) {
+          return false;
+        }
+      default: 
+        return !!isRevealed;
+    }
+  }
+
+  public static async setRevealed(item: Item<any>, value: boolean | null): Promise<void> {
+    if (value == null) {
+      await item.unsetFlag(staticValues.moduleName, 'revealed');
+    } else {
+      await item.setFlag(staticValues.moduleName, 'revealed', value);
+    }
+  }
+
+  public static async getAbilitiesHtml(actorId: string): Promise<string> {
+    const actor = game.actors.get(actorId);
+  
+    const identifiableAbilities: IdentifiableAbility[] = Array.from(actor.items)
+      .filter(item => Identifiable.isIdentifiable(item))
+      .sort((a: Item, b: Item) => a.name.localeCompare(b.name))
+      .map((item: Item<any>) => {
+        return {
+          actorId: actorId,
+          ownedItemId: item.id,
+          name: item.name,
+          img: item.img,
+          revealed: Identifiable.isRevealed(item)
+        }
+      });
+  
+    return await (renderTemplate(`modules/${staticValues.moduleName}/templates/identifiable-abilities.hbs`, {
+      items: identifiableAbilities
+    }) as any);
+  }
+  
+  public static async printAbilities(actorId: string): Promise<void> {
+    const actor = game.actors.get(actorId);
+    const htmlTemplate: string = await Identifiable.getAbilitiesHtml(actorId);
+  
+    return await ChatMessage.create({
+      speaker: ChatMessage.getSpeaker({actor: actor}),
+      content: htmlTemplate,
+      type: CONST.CHAT_MESSAGE_TYPES.OTHER,
+      user: game.userId
+    }).then(() => null);
+  }
+
+}
